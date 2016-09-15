62a93fe7f89b701241940ed89c25a66213a15f8d
ruben dominguez presa <rubendominguezpresa@hotmail.com>
2016-09-15 16:57:01 +0200

    wrod_sorting funcionalv1

--- word_sorting.rb
+++ word_sorting.rb
@@ -1,19 +1,21 @@
+#require 'pry'
 frase={}
 def ordena(frase)
 	#puts frase
 	for pos in 0..frase.length
 		menor=pos
 		#puts menor
-		for pos2 in 1..frase.length-1
+		for pos2 in 1..frase.length
 			#puts frase[pos2+1]
 			if ((frase[pos2].to_s.upcase)<(frase[menor].to_s.upcase))
 				menor=pos2
 			end
-
+			#binding.pry
 			if (pos!=menor)
 				aux=frase[pos]
 				frase[pos]=frase[menor]
 				frase[menor]=aux
+				#binding.pry
 			end
 		end
 	end
